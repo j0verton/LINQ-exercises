e46e3521a85934d8c06f65d2f6f97a3f5575f99c
Joe <overtonje@gmail.com>
2020-11-19 12:02:21 -0600

    added comments grouping exercises and plugged in partitioning exercise base info

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace linq
     {
         static void Main(string[] args)
         {
+            //// Restriction/Filtering Operations
             // // Find the words in the collection that start with the letter 'L'
             // List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
 
@@ -23,7 +24,7 @@ namespace linq
 
             // List<int> fourSixMultiples = numbers.Where(number => number % 4 == 0 || number % 6 == 0).ToList();
             // fourSixMultiples.ForEach(num => Console.WriteLine(num));
-
+            // // Ordering Operations
             // //Order these student names alphabetically, in descending order (Z to A)
             //             List<string> names = new List<string>()
             // {
@@ -43,28 +44,42 @@ namespace linq
             //             };
             //             List<int> sortedNums = numbers.OrderBy(n => n).ToList();
             //             sortedNums.ForEach(num => Console.WriteLine(num));
+            //// Aggregate Operations
+            //             // Output how many numbers are in this list
+            //             List<int> numbers = new List<int>()
+            // {
+            //     15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            // };
+            //             Console.WriteLine(numbers.Count());
+            //             // How much money have we made?
+            //             List<double> purchases = new List<double>()
+            // {
+            //     2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            // };
+            //             Console.WriteLine(purchases.Sum());
 
-            // Output how many numbers are in this list
-            List<int> numbers = new List<int>()
+            //             // What is our most expensive product?
+            //             List<double> prices = new List<double>()
+            // {
+            //     879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            // };
+
+
+            //             Console.WriteLine(prices.Max());
+
+            //// Partitioning Operations
+            List<int> wheresSquaredo = new List<int>()
 {
-    15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+    66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
 };
-            Console.WriteLine(numbers.Count());
-            // How much money have we made?
-            List<double> purchases = new List<double>()
-{
-    2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
-};
-            Console.WriteLine(purchases.Sum());
+            /*
+                Store each number in the following List until a perfect square
+                is detected.
 
-            // What is our most expensive product?
-            List<double> prices = new List<double>()
-{
-    879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
-};
+                Expected output is { 66, 12, 8, 27, 82, 34, 7, 50, 19, 46 } 
 
-
-            Console.WriteLine(prices.Max());
+                Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+            */
         }
     }
 }
